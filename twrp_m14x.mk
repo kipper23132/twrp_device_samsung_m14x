aaea43dd58cbec4ca0074a47da52cbe290cd15f5
kipper23132 <156115024+kipper23132@users.noreply.github.com>
2024-10-14 13:58:32 +0530

    Update twrp_m14x.mk

--- twrp_m14x.mk
+++ twrp_m14x.mk
@@ -23,3 +23,20 @@ PRODUCT_NAME := twrp_m14x
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-M146B
 PRODUCT_MANUFACTURER := samsung
+
+# SHRP
+
+SHRP_PATH := device/samsung/m14x
+SHRP_MAINTAINER := kip
+SHRP_DEVICE_CODE := m14x
+SHRP_EDL_MODE := 1
+SHRP_EXTERNAL := /external_sd
+SHRP_INTERNAL := /sdcard
+SHRP_OTG := /usb_otg
+SHRP_FLASH := 1
+SHRP_REC := /dev/block/platform/13540000.dwmmc0/by-name/RECOVERY
+SHRP_REC_TYPE := Treble
+SHRP_DEVICE_TYPE := A_Only
+SHRP_DARK := true
+SHRP_OFFICIAL := true
+SHRP_EXPRESS := true
